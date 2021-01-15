ec15aa779a3ca39874f7cd0c37afbbd1c02eba71
Nate D <nate@idontchop.com>
2021-01-14 16:35:57 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hand Range Equity Microservice
+In Development. Returns hand range poker equities when supplied a text string representing a range of poker hands (TT+, ATs+, AQo+)
---
+++ src/main/java/com/idontchop/handequityservice/HandEquityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.idontchop.handequityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandEquityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandEquityServiceApplication.class, args);
+	}
+
+}
