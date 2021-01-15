f5f9b98435f5a9943f2efcfda27745f87588d44a
Nate D <nate@idontchop.com>
2021-01-14 16:58:21 -0800

    config setup

---
+++ src/main/java/com/idontchop/handequityservice/controllers/MainController.java
@@ -0,0 +1,46 @@
+package com.idontchop.handequityservice.controllers;
+
+import java.net.NetworkInterface;
+import java.net.SocketException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import dtos.RestMessage;
+
+@RestController
+public class MainController {
+	
+	@Value ("${server.port}")
+	private String serverPort;
+	
+	@Value("${spring.application.name}")
+	private String appName;
+	
+	@RequestMapping(value = "/helloWorld", method = { RequestMethod.GET, RequestMethod.POST} )
+	public RestMessage helloWorld () {
+		String serverAddress,serverHost;
+		try {
+			serverAddress = NetworkInterface.getNetworkInterfaces().nextElement()
+					.getInetAddresses().nextElement().getHostAddress();
+		} catch (SocketException e) {
+			serverAddress = e.getMessage();
+		}
+		try {
+			serverHost = NetworkInterface.getNetworkInterfaces().nextElement()
+					.getInetAddresses().nextElement().getHostName();
+		} catch (SocketException e) {
+			serverHost = e.getMessage();
+		}
+        
+		return RestMessage.build("Hello from " + appName)
+				.add("service", appName)
+				.add("host", serverHost)
+				.add("address", serverAddress)
+				.add("port", serverPort);			
+	}
+	
+
+}
